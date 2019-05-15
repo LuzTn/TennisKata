d57f5ce32ac88f7bd767f8e2e437fdb8feede174
Nour <nour.sboui1993@gmail.com>
2019-05-15 10:11:01 +0200

    [SPRINT1-US1] : manage a tennis GAME within a set of a tennis match

---
+++ src/TennisKata.java
@@ -0,0 +1,69 @@
+public class TennisKata {
+
+    private String player1;
+    private String player2;
+    private int p1Score = 0;
+    private int p2Score = 0;
+
+    public TennisKata(String player1, String player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+
+    public String getScore() {
+        if (hasWon()) {
+            return playerWithHighestScore() + " wins";
+        }
+
+        if (hasAdvantage()) {
+            return "Advantage " + playerWithHighestScore();
+        }
+
+
+        if (p1Score == p2Score) {
+            return "Both players: " + tennisScore(p1Score);
+        }
+
+        return tennisScore(p1Score) + "," + tennisScore(p2Score);
+    }
+
+    private String playerWithHighestScore() {
+       return (p1Score > p2Score) ? player1 : player2;
+    }
+
+    private boolean hasWon() {
+        if ((p2Score >= 4 && p2Score >= p1Score + 2) || (p1Score >= 4 && p1Score >= p2Score + 2))
+            return true;
+        return false;
+    }
+
+    private boolean hasAdvantage() {
+        if ((p2Score >= 4 && p2Score == p1Score + 1) || (p1Score >= 4 && p1Score == p2Score + 1))
+            return true;
+
+        return false;
+
+    }
+
+    public void p1Score() {
+        p1Score++;
+    }
+
+    public void p2Score() {
+        p2Score++;
+    }
+
+    private int tennisScore(int score) {
+        switch (score) {
+            case 0:
+                return 0;
+            case 1:
+                return 15;
+            case 2:
+                return 30;
+            case 3:
+                return 40;
+        }
+        throw new IllegalArgumentException("Illegal score: " + score);
+    }
+}
---
+++ src/TennisKataTest.java
@@ -0,0 +1,121 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TennisKataTest {
+
+    TennisKata game = new TennisKata("Player 1", "Player 2");
+
+    @Test
+    public void testNewGameStartsWithScoreZero() {
+        String score = game.getScore();
+        assertEquals("Both players: " + 0, score);
+        System.out.println(score);
+    }
+
+    @Test
+    public void testPlayerOneWinsFirstBall() {
+        game.p1Score();
+        String score = game.getScore();
+        assertEquals(15 + "," + 0, score);
+        System.out.println(score);
+    }
+
+    @Test
+    public void testFifteenBoth() {
+        game.p1Score();
+        game.p2Score();
+        String score = game.getScore();
+        assertEquals("Both players: " + 15, score);
+        System.out.println(score);
+    }
+
+    @Test
+    public void testPlayerTwoWinsFirstTwoBalls() {
+        createScore(0, 2);
+        String score = game.getScore();
+        assertEquals(0 + "," + 30, score);
+        System.out.println(score);
+    }
+
+    @Test
+    public void testPlayerOneWinsFirstThreeBalls() {
+        createScore(3, 0);
+        String score = game.getScore();
+        assertEquals(40 + "," + 0, score);
+        System.out.println(score);
+    }
+
+    /*@Test
+    public void testPlayersAreDeuce() {
+        createScore(3, 3);
+
+        String score = game.getScore();
+        assertEquals("Deuce", score);
+    }
+*/
+    @Test
+    public void testPlayerOneWinsGame() {
+        createScore(4, 0);
+        String score = game.getScore();
+        assertEquals("Player 1 wins", score);
+        System.out.println(score);
+    }
+
+    @Test
+    public void testPlayerTwoWinsGame() {
+        createScore(1, 4);
+        String score = game.getScore();
+        assertEquals("Player 2 wins", score);
+        System.out.println(score);
+    }
+
+    @Test
+    public void testPlayerTwoAdvantage() {
+        createScore(4, 5);
+        String score = game.getScore();
+        assertEquals("Advantage Player 2", score);
+        System.out.println(score);
+    }
+
+    @Test
+    public void testPlayerOneAdvantage() {
+        createScore(5, 4);
+        String score = game.getScore();
+        assertEquals("Advantage Player 1", score);
+        System.out.println(score);
+    }
+
+    @Test
+    public void testPlayerTwoWins() {
+        createScore(2, 4);
+        String score = game.getScore();
+        assertEquals("Player 2 wins", score);
+        System.out.println(score);
+    }
+
+    @Test
+    public void testPlayerTwoWinsAfterAdvantage() {
+        createScore(6, 8);
+        String score = game.getScore();
+        assertEquals("Player 2 wins", score);
+        System.out.println(score);
+    }
+
+    @Test
+    public void testPlayerOneWinsAfterAdvantage() {
+        createScore(8, 6);
+        String score = game.getScore();
+        assertEquals("Player 1 wins", score);
+        System.out.println(score);
+    }
+
+    private void createScore(int playerOneBalls, int playerTwoBalls) {
+        for (int i = 0; i < playerOneBalls; i++) {
+            game.p1Score();
+        }
+        for (int i = 0; i < playerTwoBalls; i++) {
+            game.p2Score();
+        }
+    }
+}
